9f9815fb102417f0621b38b92ac857c8a55e3629
foj-1705 <81002667+foj-1705@users.noreply.github.com>
2022-07-14 14:42:38 -0500

    Update angular.py

--- angular.py
+++ angular.py
@@ -9,12 +9,7 @@ def minmax_ang(logit, logit_adv, label):
     
     cos_n = torch.gather(logit, 1, torch.unsqueeze(label, 1))
     
-    cos_a = torch.gather(logit_adv, 1, torch.unsqueeze(label, 1))
-
-    #cos_a = torch.max(cos_a, dim=1)[0]
-
-    #cos_n = torch.max(cos_n, dim=1)[0]
-    
+    cos_a = torch.gather(logit_adv, 1, torch.unsqueeze(label, 1))    
     
 
     
