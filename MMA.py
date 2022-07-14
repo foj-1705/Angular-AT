852e781426803417cc7d920ca0dc795f2c74fe18
foj-1705 <81002667+foj-1705@users.noreply.github.com>
2022-07-14 14:39:18 -0500

    Update MMA.py

--- MMA.py
+++ MMA.py
@@ -12,16 +12,20 @@ def get_mma_loss(weight):
 
     # computing cosine similarity: dot product of normalized weight vectors
     weight_ = F.normalize(weight, p=2, dim=1)
-    cosine = torch.matmul(weight_, weight_.t())
+    cosine = torch.matmul(weight_, weight_.t())     #+ 1
 
     # make sure that the diagnonal elements cannot be selected
-    cosine = cosine - 2. * torch.diag(torch.diag(cosine))
+    cosine = cosine - 3. * torch.diag(torch.diag(cosine))
 
     # maxmize the minimum angle
-    loss = -torch.acos(cosine.max(dim=1)[0].clamp(-0.99999, 0.99999)).mean()
 
-    #loss = cosine.max(dim=1)[0].clamp(-0.99999, 0.99999).mean()
-    return loss
+    #loss = -torch.acos(cosine.max(dim=1)[0].clamp(-0.99999, 0.99999)).mean()
+    #loss = -1 * torch.acos(cosine.max(dim=1)[0].clamp(-0.99999, 0.99999))   #.mean()
+
+    
+
+    loss = cosine.max(dim=1)[0]
+    return loss.mean()
 
 
 
